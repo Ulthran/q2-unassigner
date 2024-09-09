6056df901fa428aa3b5180d301e701e1b8219eba
Ulthran <ctbushman@gmail.com>
2024-09-09 15:28:12 -0400

    Init

---
+++ q2_unassigner/plugin_setup.py
@@ -0,0 +1,41 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2024, Charlie Bushman.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
+from qiime2.plugin import Citations, Plugin
+from q2_types.feature_table import FeatureTable, Frequency
+from q2_unassigner import __version__
+from q2_unassigner._methods import duplicate_table
+
+citations = Citations.load("citations.bib", package="q2_unassigner")
+
+plugin = Plugin(
+    name="unassigner",
+    version=__version__,
+    website="https://github.com/PennChopMicrobiomeProgram/unassigner",
+    package="q2_unassigner",
+    description="Evaluate consistency with named bacterial species for short 16S rRNA marker gene sequences",
+    short_description="Unassigner",
+    # The plugin-level citation of 'Caporaso-Bolyen-2024' is provided as
+    # an example. You can replace this with citations to other references
+    # in citations.bib.
+    citations=[citations['Caporaso-Bolyen-2024']]
+)
+
+plugin.methods.register_function(
+    function=duplicate_table,
+    inputs={'table': FeatureTable[Frequency]},
+    parameters={},
+    outputs=[('new_table', FeatureTable[Frequency])],
+    input_descriptions={'table': 'The feature table to be duplicated.'},
+    parameter_descriptions={},
+    output_descriptions={'new_table': 'The duplicated feature table.'},
+    name='Duplicate table',
+    description=("Create a copy of a feature table with a new uuid. "
+                 "This is for demonstration purposes only. 🧐"),
+    citations=[]
+)
